06badff2c8f4f2b42f8ab32ab60e21c8c2431d59
Andrew Kelley <superjoe30@gmail.com>
2013-12-16 16:02:00 -0500

    fix some stuff
     * remove node_modules
     * add swig to package.json
     * fix routes
     * fix client side crash

--- sokotoad/routes/index.js
+++ sokotoad/routes/index.js
@@ -6,3 +6,19 @@
 exports.index = function(req, res){
   res.render('index');
 };
+
+exports.saveLevel = function(req, res) {
+    MongoClient.connect("mongodb://localhost:27017/sokotoadDB", function(err, db) {
+        if(err) throw err;
+        var levelsCollection = db.collection("levels");
+        levelsCollection.insert( {"levelData": req.body.level}, function(err, inserted) {
+            if(err) throw err;
+            console.log("Level successfuly inserted");
+            res.render('levelSaved', {levelData: level});            
+        });
+    });
+};
+
+exports.levelCreator = function(req, res) {
+  res.render('levelCreator');
+};
--- sokotoad/routes/levelCreator.js
+++
@@ -1,8 +0,0 @@
-
-/*
- * GET home page.
- */
-
-exports.levelCreator = function(req, res){
-  res.render('levelCreator');
-};
