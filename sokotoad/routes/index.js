8eadb5617ea719922181381d7c781f4e8e177dea
Solveig Hansen <solveig.hansen@gmail.com>
2013-12-16 16:43:35 -0500

    Fixed index.js (routes) and cleaned up a couple of things

--- sokotoad/routes/index.js
+++ sokotoad/routes/index.js
@@ -1,4 +1,4 @@
-
+var MongoClient = require('mongodb').MongoClient;
 /*
  * GET home page.
  */
@@ -10,8 +10,9 @@ exports.index = function(req, res){
 exports.saveLevel = function(req, res) {
     MongoClient.connect("mongodb://localhost:27017/sokotoadDB", function(err, db) {
         if(err) throw err;
+        var level = req.body.level;
         var levelsCollection = db.collection("levels");
-        levelsCollection.insert( {"levelData": req.body.level}, function(err, inserted) {
+        levelsCollection.insert( {"levelData": level}, function(err, inserted) {
             if(err) throw err;
             console.log("Level successfuly inserted");
             res.render('levelSaved', {levelData: level});            
